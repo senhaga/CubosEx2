3e4ca12a6007329665cece7e4fadbe5981fe0099
Mateus Ribeiro Lima <mateus.lima@jusbrasil.com.br>
2022-02-17 20:12:22 -0300

    Final exercises

---
+++ variable_operators2.js
@@ -0,0 +1,41 @@
+let x = 10;
+
+x += 5;
+//Equivalent to x = x + 5
+console.log(x);
+
+x -= 5;
+//Equivalent to x = x - 5
+console.log(x);
+
+x *= 5
+//Equivalent to x = x * 5
+console.log(x);
+
+x /= 5;
+//Equivalent to x = x / 5
+console.log(x);
+
+//------xxx------
+x = x % 3; //modulo operator
+console.log(x);
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
+
+let z = x++; // z gets value of x before adding 1 to itself
+console.log(x, z);
+
+let y = ++x; // y gets value opf x after adding 1 to itself
+console.log(x, y);
+
+
+
+
+
+
+
+
---
+++ variable_sqrt_pow.js
@@ -0,0 +1,5 @@
+let x = Math.sqrt(9), y = Math.pow(x, 3), z = Math.sqrt(-1);
+
+//power alternative to Math.pow is x**y (as is in python)
+
+console.log(x, y, z)
